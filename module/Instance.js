e05e4f7d10e824a9e2834b48bf2b0fdff3d2e69d
yinlo <longfei.yin@riveretech.com>
2022-07-07 23:12:06 +0800

    feat(modul): 优化命名
    1. 优化命名

--- module/Instance.js
+++ module/Instance.js
@@ -25,14 +25,14 @@ class Instance {
                 loaderUtils.sceneLoader.setBinaryGeometryBuffer(binaryGeometryBuffer);
             }
             // 加载场景
-            loaderUtils.loadScene(data + name + (callback || '.json')).spread(function(sceneParm, json) {
+            loaderUtils.loadScene(data + name + (callback || '.json')).spread(function(sceneParam, json) {
                 // 声明相机
                 var camera;
                 // 置空物料信息
-                sceneParm.materials = {};
+                sceneParam.materials = {};
                 // 如果有相机，则获取相机
-                if (sceneParm.cameras && sceneParm.cameras.length > 0) {
-                    camera = sceneParm.cameras[0];
+                if (sceneParam.cameras && sceneParam.cameras.length > 0) {
+                    camera = sceneParam.cameras[0];
                 }
                 // 设置相机的面为当前计算机可视区域的宽/除以可视区域的高
                 if (camera) {
@@ -48,62 +48,62 @@ class Instance {
                 var step = 10;
                 // 网格辅助线
                 var grid = new THREE.GridHelper(size, step);
-                sceneParm.add(grid);
+                sceneParam.add(grid);
                 // 辅助线
                 var axesHelper = new THREE.AxesHelper(5);
-                sceneParm.add(axesHelper);
-                sceneParm.dirLights = [];
+                sceneParam.add(axesHelper);
+                sceneParam.dirLights = [];
                 // 横过
-                sceneParm.traverse(function(camera) {
+                sceneParam.traverse(function(camera) {
                     if (camera instanceof THREE.DirectionalLight) {
                         camera.position.set(0, 0, 5);
                         camera.quaternion.normalize();
                         camera.position.applyQuaternion(camera.quaternion);
                         camera.quaternion.set(0, 0, 0, 0);
                         camera.scale.set(1, 1, 1);
-                        sceneParm.dirLights.push(camera);
+                        sceneParam.dirLights.push(camera);
                     }
                 });
                 // 动画Mixer
-                var mixer = new THREE.AnimationMixer(sceneParm);
+                var mixer = new THREE.AnimationMixer(sceneParam);
                 var i = 0;
-                for (; i < sceneParm.animations.length; i++) {
-                    mixer.clipAction(sceneParm.animations[i]).play();
+                for (; i < sceneParam.animations.length; i++) {
+                    mixer.clipAction(sceneParam.animations[i]).play();
                 }
                 // 遍历给定的AST树。
-                sceneParm.traverse(function(options) {
+                sceneParam.traverse(function(options) {
                     var material = options.material;
                     if (material && material.aoMap) {
                         !material.map;
                     }
                 });
-                sceneParm.traverse(function(box1) {
+                sceneParam.traverse(function(box1) {
                     if ('Line' === box1.name) {
                         box1.material.linewidth = 10;
                         box1.material.color.setRGB(1, 0, 1);
                     }
                 });
-                sceneParm.traverse(function(node) {
+                sceneParam.traverse(function(node) {
                     if (node instanceof THREE.SpotLight) {
                         var p = new THREE.Vector3(0, 0, -1);
                         var sprite = new THREE.Object3D;
                         node.updateMatrixWorld();
                         node.localToWorld(p);
                         sprite.position.copy(p);
-                        sceneParm.add(sprite);
+                        sceneParam.add(sprite);
                         node.target = sprite;
                     }
                     if (node.material) {
                         if (node.material.materials) {
                             node.material.materials.forEach(function(b) {
-                                sceneParm.materials[b.uuid] = b;
+                                sceneParam.materials[b.uuid] = b;
                             });
                         } else {
-                            sceneParm.materials[node.material.uuid] = node.material;
+                            sceneParam.materials[node.material.uuid] = node.material;
                         }
                     }
                 });
-                resolve(sceneParm);
+                resolve(sceneParam);
             });
         });
     }
--- module/utils/FileLoaderUtils.js
+++ module/utils/FileLoaderUtils.js
@@ -1,43 +1,45 @@
 import {FileLoader} from "three";
 import {DefaultLoadingManager} from "three";
 
-/**
- * 处理光的漫反射/环境光,球谐函数系数
- * @param jsonData
- * @returns {number[]}
- */
-function sphericalHarmonicsCoefficients(jsonData) {
-    debugger
-    // 拷贝元素，从0到26个元素,球谐波离线预计算的系数因子
-    var data = jsonData.slice(0, 27);
-    // 我猜应该是9个系数
-    // 通过预先计算出的常数来优化漫反射
-    // 伴随勒让德多项式 常量
-    var a = 1 / (2 * Math.sqrt(Math.PI));
-    var e = -(.5 * Math.sqrt(3 / Math.PI));
-    var i = -e;
-    var abcd = e;
-    var knobHalf = .5 * Math.sqrt(15 / Math.PI);
-    var currentRelations = -knobHalf;
-    var c = .25 * Math.sqrt(5 / Math.PI);
-    var addedRelations = currentRelations;
-    var l = .25 * Math.sqrt(15 / Math.PI);
-    var array = [a, a, a, e, e, e, i, i, i, abcd, abcd, abcd, knobHalf, knobHalf, knobHalf, currentRelations, currentRelations, currentRelations, c, c, c, addedRelations, addedRelations, addedRelations, l, l, l];
-    return array.map(function(position, i) {
-        return position * data[i];
-    });
-}
+
 class FileLoaderUtils extends FileLoader{
     constructor(data){
         super();
         this.manager = undefined !== data ? data : DefaultLoadingManager;
     }
     load(url, loadCallback, onProgress, onError) {
+        let sphericalHarmonicsCoefficients = this.sphericalHarmonicsCoefficients;
         super.load(url, function(data) {
             const jsonData = JSON.parse(data);
             const x = sphericalHarmonicsCoefficients(jsonData);
             loadCallback(x);
         }, onProgress, onError);
+    }
+    /**
+     * 处理光的漫反射/环境光,球谐函数系数
+     * @param jsonData
+     * @returns {number[]}
+     */
+    sphericalHarmonicsCoefficients(jsonData) {
+        debugger
+        // 拷贝元素，从0到26个元素,球谐波离线预计算的系数因子
+        var data = jsonData.slice(0, 27);
+        // 我猜应该是9个系数
+        // 通过预先计算出的常数来优化漫反射
+        // 伴随勒让德多项式 常量
+        var a = 1 / (2 * Math.sqrt(Math.PI));
+        var e = -(.5 * Math.sqrt(3 / Math.PI));
+        var i = -e;
+        var abcd = e;
+        var knobHalf = .5 * Math.sqrt(15 / Math.PI);
+        var currentRelations = -knobHalf;
+        var c = .25 * Math.sqrt(5 / Math.PI);
+        var addedRelations = currentRelations;
+        var l = .25 * Math.sqrt(15 / Math.PI);
+        var array = [a, a, a, e, e, e, i, i, i, abcd, abcd, abcd, knobHalf, knobHalf, knobHalf, currentRelations, currentRelations, currentRelations, c, c, c, addedRelations, addedRelations, addedRelations, l, l, l];
+        return array.map(function(value, index) {
+            return value * data[index];
+        });
     }
 }
 
