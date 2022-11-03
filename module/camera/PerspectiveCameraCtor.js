eae22123489c013087fe5bfcf76720bc265d2f05
yinlo <longfei.yin@riveretech.com>
2022-11-03 16:08:19 +0800

    feat(camera): 优化透视相机部分的描述
    1. 增加透视相机描述及代码示例部分

--- module/camera/PerspectiveCameraCtor.js
+++ module/camera/PerspectiveCameraCtor.js
@@ -4,6 +4,21 @@ import {PerspectiveCamera} from "three";
 var ctor = THREE.PerspectiveCamera;
 var value = new THREE.Vector3;
 
+/**
+ * 透视相机构造函数
+ * @constructor
+ * @param fov {number} 视角
+ * @param aspect {number} 宽高比
+ * @param near {number} 近裁剪面
+ * @param far {number} 远裁剪面
+ * @extends PerspectiveCamera
+ * @example
+ * var camera = new PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 1000);
+ * camera.position.set(0, 0, 100);
+ * camera.lookAt(new THREE.Vector3(0, 0, 0));
+ * scene.add(camera);
+ * @example
+ */
 class PerspectiveCameraCtor extends PerspectiveCamera{
     constructor(fov,aspect, near, far) {
         super(fov,aspect, near, far);
@@ -12,8 +27,8 @@ class PerspectiveCameraCtor extends PerspectiveCamera{
          * 更新高度
          */
         this.updateHeight = function() {
-            var x = 1000;
-            var vertCoords = -100;
+            let x = 1000;
+            const vertCoords = -100;
             return function(i, canCreateDiscussions) {
                 i = i * vertCoords;
                 x = x + i;
