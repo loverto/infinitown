aa5f4b1023c6bdfc99890b73d0a235b5a43aae56
yinlo <longfei.yin@riveretech.com>
2022-11-04 16:32:39 +0800

    feat(model): 优化可移动3D模型最后位置的记录
    1. 优化可移动3D模型最后位置的记录，可以修复偶尔碰撞检测失灵的问题

--- module/model/BaseMoveTableModel.js
+++ module/model/BaseMoveTableModel.js
@@ -1,24 +1,28 @@
 import * as THREE  from 'three';
 import globalConfig from 'module/config/GlobalConfig';
-import Utils from 'module/utils/Utils';
+import SeedrandomUtils from 'module/utils/SeedrandomUtils';
 import {Object3D} from "three";
 // 最小的缓存的行数60x9=540
 var MIN_BUFFER_ROWS = globalConfig.CHUNK_SIZE * globalConfig.TABLE_SIZE;
 // 欧几里得距离算法的工具
 var distance = THREE.Math.euclideanModulo;
 
+/**
+ * 基础的移动的表格模型,用于移动的3D模型
+ * 记录移动的模型的表格位置、表格的最后位置，以及3D模型的最后位置
+ */
 class BaseMoveTableModel extends  Object3D{
     /**
      * 可以移动的3d对象，该如何移动
-     * @param data
+     * @param tableData 表格数据
      * @constructor
      */
-    constructor(data) {
+    constructor(tableData) {
         super()
         // 前一个块的值
         this.previousChunk = null;
         // 设置当前的表格信息
-        this.table = data;
+        this.table = tableData;
         // 表格位置
         this.tablePosition = new THREE.Vector3;
         // 表格最后的位置
@@ -33,7 +37,7 @@ class BaseMoveTableModel extends  Object3D{
      */
     _updateTablePosition() {
         // 获取表格位置的值
-        Utils.getTablePosition(this.position, this.parent.tableX, this.parent.tableY, this.tablePosition);
+        SeedrandomUtils.getTablePosition(this.position, this.parent.tableX, this.parent.tableY, this.tablePosition);
         // 如果表格最后位置为空，则用当前的表格位置给其赋值
         if (0 === this.lastTablePosition.length()) {
             this.lastTablePosition.copy(this.tablePosition);
@@ -42,7 +46,7 @@ class BaseMoveTableModel extends  Object3D{
         const x = this.tablePosition.x - this.lastTablePosition.x;
         // 当前表格的坐标的z值与最后表格z值的差
         const z = this.tablePosition.z - this.lastTablePosition.z;
-        // 把当前表格的位置信息赋值给最后表格
+        // 把当前表格的位置信息复制给最后表格
         this.lastTablePosition.copy(this.tablePosition);
         // 计算x坐标与最小缓存行的距离
         let xd = Math.floor(distance(this.tablePosition.x + 40, MIN_BUFFER_ROWS) / globalConfig.CHUNK_SIZE);
@@ -58,11 +62,10 @@ class BaseMoveTableModel extends  Object3D{
             console.log('warp on Z', z, context.tableY, node.tableY)
         }
         //上一个块
-        if ( this.previousChunk !== context){
-            context !== node
+        if ( this.previousChunk !== context && context !== node){
+            //最后的位置
+            this.lastPosition.copy(this.position)
         }
-        //最后的位置
-        this.lastPosition.copy(this.position)
         // 当前块不能是父块
         if (node !== context) {
             node.add(this);
